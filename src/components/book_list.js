74bb45266c3d74d227ffa275f5265e91f776f96c
lgants <lgants@gmail.com>
2016-10-05 14:06:31 -0400

    first version of stylized app

--- src/components/book_list.js
+++ src/components/book_list.js
@@ -5,20 +5,32 @@ import { Link } from 'react-router';
 
 function BookList(props){
   return (
-    <div className="col-md-4">
-      <button role="button" className="btn btn-secondary btn-block" type="button">
-        <Link to="/books/new">Add a book :)</Link>
-      </button>
-      <ul className="list-group">
-        {props.books.map(book =>
-          <li className="list-group-item" key={book.id}>
-          <Link to={`/books/${book.id}`}>
-          {book.title}
-          </Link>
-          </li>
+    <div className="col-lg-4 col-md-4 col-sm-4">
+      <div id="book-list-container" className="panel panel-default">
+        <div className="panel-body">
+          <button role="button" className="btn btn-default btn-block" type="button">
+            <Link to="/books/new">Add a book :)</Link>
+          </button>
+          <br />
+          <div className="panel panel-default">
+            <div className="panel-heading">Books</div>
+            <ul id="book-scroll-list-group" className="list-group">
+              {props.books.map((book) =>
+                <li className="list-group-item" key={book.id}>
+                  <Link to={`/books/${book.id}`}>
+                    <h4 className="list-group-item-heading">{book.title}</h4>
+                    <p className="list-group-item-text">...</p>
+                  </Link>
+                </li>
+              )}
+            </ul>
+          </div>
+        </div>
+      </div>
 
-        )}
-      </ul>
+
+
+
     </div>
   )
 }
