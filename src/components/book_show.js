6f394d10f8b1dfc5e8bb7dabaf95635013901e54
Francis Rocco <roccofrancis@gmail.com>
2016-10-06 08:55:27 -0400

    add delete book functionality

--- src/components/book_show.js
+++ src/components/book_show.js
@@ -1,30 +1,46 @@
 import React, { Component } from 'react';
-import {connect} from 'react-redux';
+import * as actions from '../actions/book_actions'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux';
 import { Link } from 'react-router';
 
-function BookShow(props){
-  return (
-    <div className='col-sm-8 col-md-8 col-lg-8' >
-      <div id="book-show-container" className="panel panel-default">
-        <div className="panel-body">
-          <h2>{props.book.title}</h2>
-          <h4>{props.book.description}</h4>
-          <br />
-          <div className="panel panel-default">
-            <div className="panel-heading">Books</div>
-            <ul id="chapter-scroll-list-group" className="list-group">
-              {props.book.chapters.map((chapter) =>
-                <Link to={`/books/${props.book.id}/chapters/${chapter.id}`}><li className="list-group-item">
-                  <h4 className="list-group-item-heading">{chapter.title}</h4>
-                  <p className="list-group-item-text">...</p>
-                </li></Link>
-              )}
-            </ul>
+class BookShow extends React.Component {
+  constructor(props) {
+    super(props)
+    this.deleteBookHandler = this.deleteBookHandler.bind(this)
+  }
+
+  deleteBookHandler(event) {
+    const deleteBook = this.props.book
+    this.props.actions.deleteBook(deleteBook)
+  }
+
+  render() {
+    return (
+      <div className='col-sm-8 col-md-8 col-lg-8' >
+        <div id="book-show-container" className="panel panel-default">
+          <div className="panel-body">
+            <h2>{this.props.book.title}</h2>
+            <h4>{this.props.book.description}</h4>
+            <button onClick={this.deleteBookHandler}>Delete Book </button>
+            <br />
+            <br />
+            <div className="panel panel-default">
+              <div className="panel-heading">Chapters</div>
+              <ul id="chapter-scroll-list-group" className="list-group">
+                {this.props.book.chapters.map((chapter) =>
+                  <Link to={`/books/${this.props.book.id}/chapters/${chapter.id}`}><li className="list-group-item">
+                    <h4 className="list-group-item-heading">{chapter.title}</h4>
+                    <p className="list-group-item-text">...</p>
+                  </li></Link>
+                )}
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 function mapStateToProps(state, ownProps) {
@@ -36,5 +52,9 @@ function mapStateToProps(state, ownProps) {
   }
 }
 
-const componentCreator = connect(mapStateToProps)
+function mapDispatchToProps(dispatch) {
+  return {actions: bindActionCreators(actions, dispatch)}
+}
+
+const componentCreator = connect(mapStateToProps, mapDispatchToProps)
 export default componentCreator(BookShow);
