f5120e68ac10993b1722ca2ed85bd6a1482978b7
Francis Rocco <roccofrancis@gmail.com>
2016-10-09 19:11:52 -0400

    add reroute if login unsuccessful

--- src/reducers/session_reducer.js
+++ src/reducers/session_reducer.js
@@ -4,7 +4,11 @@ import {browserHistory} from 'react-router'
 export default function sessionReducer(state=[], action) {
   switch (action.type) {
     case 'LOG_IN_SUCCESS':
-    browserHistory.push('/books')
+      if (sessionStorage.jwt == "undefined") {
+        browserHistory.push('/login')
+      } else {
+        browserHistory.push('/books')
+      }
     default:
     return state
   }
